ed85b37f7042db46e6371c5689ba809f6648d1ee
AlmirJusic <74711786+AlmirJusic@users.noreply.github.com>
2020-11-19 12:11:41 +0100

    Add files via upload

---
+++ Task2-Loading/script.js
@@ -0,0 +1,11 @@
+function clickme()
+        {
+            var krug=document.getElementById("loading-border");
+            
+            if(krug.style.animationDirection==="normal")
+            krug.style.animationDirection="reverse";
+            else
+            krug.style.animationDirection="normal";
+
+            
+        }
